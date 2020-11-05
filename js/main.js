e736036d6faac73bafa843c345eee8f10677ca24
christensencaleigh <73854391+christensencaleigh@users.noreply.github.com>
2020-11-05 16:53:25 -0600

    Becoming a Javascript wizard

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// console.log("Hello from the javascript console!");
+//
+// alert("You just won a new Ipad!");
+//
+// var IpadWinner = "You just won an ipad!";
+//
+// alert(IpadWinner);
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
