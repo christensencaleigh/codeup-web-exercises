a34dae4bc3987d8bcfa14efb5ddfc45d831dcc59
Caleigh Christensen <christensen.caleigh@gmail.com>
2020-12-17 17:02:38 -0600

    first commit

---
+++ js/my-custom-javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	// alert( 'The DOM has finished loading!' );
+
+	alert( $('#main-header').html() );
+	alert($('#my-paragraph').html() );
+
+	$('.willFerrel').css('border', '1px solid red');
+});
+
+
+
+
+
