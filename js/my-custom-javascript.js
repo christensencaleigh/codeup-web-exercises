3ea15ba0be741741c4ee819654b8187ecfdb8bde
Caleigh Christensen <christensen.caleigh@gmail.com>
2020-12-18 12:23:24 -0600

    jQuery selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,11 +1,52 @@
 $(document).ready(function() {
 	// alert( 'The DOM has finished loading!' );
 
-	alert( $('#main-header').html() );
-	alert($('#my-paragraph').html() );
+//jquery selectors
 
-	$('.willFerrel').css('border', '1px solid red');
+	// //id selector
+	// alert($('#my-paragraph').html());
+	// alert($('#xmas').html() );
+	//
+	// //class selector
+	// $('.kalypso').css('border', '1px solid red');
+	//
+	// //element selectors
+	// $('li').css('font-size', '20px');
+	// alert($('h1').html());
+	//
+	// //multiple selectors
+	// $('li, h1, p').css('background-color', 'green')
+	//
+//jquery events
+
+	//change color of h1 element
+
+	$('#kalypso').click(function() {
+		$(this).css('background-color', 'green');
+	});
+
+	//make p have font size of 18px using dblclick
+
+	$('p').dblclick(function() {
+		$(this).css('font-size', '18px');
+	})
+
+	//set all li test color to red when hovering, reset to black when it is not
+
+	$('li').hover(
+		function() {
+			$(this).css('color', 'red');
+	},
+		function() {
+			$(this).css('color', 'black');
+		}
+	);
+
 });
+
+
+
+
 
 
 
