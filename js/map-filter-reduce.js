f86a6afd2d0d2803fb0c6db4b75f4ef4caf4b83c
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-13 12:32:52 -0600

    completed exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const users = [
 	{
 		id: 1,
@@ -36,25 +38,18 @@ const users = [
 	}
 ];
 
-//setting up a loop for iterating through my arrays
-
-function getLang(arr) {
-	let newArr = [];
-	for(let i = 0; i < arr.length; i++) {
-		newArr.push(arr[i])
-	}
-	return newArr.join('')
-}
 
 
-// FILTER - LANGUAGES***
+// FILTER - LANGUAGES
 
 let filterLang = users.filter(function (user) {
-	return getLang(user.languages.length) >= 3;
-})
-console.log(filterLang)
+	return user.languages.length >= 3;
+});
+// console.log(filterLang)
 
-
+//rewrite using ES6
+let filterLangES6 = users.filter(user => user.languages.length  >= 3);
+console.log(filterLangES6);
 
 
 // MAP - EMAIL ADDRESSES
@@ -69,34 +64,41 @@ let userEmailsES6 = users.map(user => user.email);
 console.log(userEmailsES6)
 
 
-
 // REDUCE - AVERAGE YEARS OF EXPERIENCE
 
 let totalYears = users.reduce(function (total,user) {
-	return total += user.yearsOfExperience / users.length;
+	return total + user.yearsOfExperience / users.length;
 }, 0)
 // console.log(totalYears);
 
 //rewrite using ES6
-let totalYearsES6 = users.reduce((total, user) => total += user.yearsOfExperience / users.length, 0)
+let totalYearsES6 = users.reduce((total, user) => total + user.yearsOfExperience / users.length, 0)
 console.log(totalYearsES6);
 
 
 // REDUCE - LONGEST EMAIL***
+let hugeEmail = users.reduce(function (currentLongest,thisUser) {
+	if (thisUser.email.length > currentLongest.email.length) {
+		//this will return a value to reassign 'currentLongest'
+		return thisUser;
+	} else {
+		//this means that the user being looked at right now does NOT have a longer email then the running champ above
+		return currentLongest;
+	}
+}, users[0])
+console.log("The user with the hugest email is: " + hugeEmail.email);
 
-let longEmail = users.reduce(function (total, user) {
-	return total += user.email.length
-}, 0)
-// console.log(longEmail);
+//rewrite using ES6
+let longestEmailUser = users.reduce((currentLongest, user) => user.email.length > currentLongest.email.length ? user : currentLongest, users[0])
+console.log(longestEmailUser.name + " has the longest email address.");
 
-
-
-// REDUCE - LIST NAMES IN A SINGLE STRING
+// REDUCE - LIST NAMES IN A SINGLE STRING***
 let newArr = [];
-let listNames = users.reduce(function (total, user) {
+let userNames = users.reduce(function (listOfNames, user) {
 	return newArr += user.name
 }, 0)
-console.log(listNames);
 
+console.log(userNames);
 
+// let userNames = users.reduce((listOfNames, user) => )
 
