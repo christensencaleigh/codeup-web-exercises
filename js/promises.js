6b0a06b12c2aab07628f6677060b0af6bfd6a2d1
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-18 21:48:59 -0600

    set up first function to get usernames

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,13 @@
-alert("Loading!")
+"use strict";
+
+// fetch(url, {headers: {'Authorization': 'token' + githubToken}});
+//
+// Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made.
+
+function getGithubUsernames() {
+	return fetch('https://api.github.com/users')
+		.then(response => response.json());
+
+}
+// console.log("working!!!!")
+// console.log(getGithubUsernames());
