95238f158487c417b09586562449e4851d5f4f93
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-21 12:22:49 -0600

    completed promises exercise

--- js/promises.js
+++ js/promises.js
@@ -9,5 +9,37 @@ function getGithubUsernames() {
 		.then(response => response.json());
 
 }
-// console.log("working!!!!")
-// console.log(getGithubUsernames());
+console.log("working!!!!")
+console.log(getGithubUsernames());
+
+function getLastPush(events) {
+	for (let event of events) {
+		if (event.type === 'PushEvent') {
+			return new Date(event.created_at).toDateString();
+		}
+	}
+	return null;
+}
+
+function lastPushDate(username) {
+	const url = `https://api.github.com/users/${username}/events`;
+	const options = {headers: {'Authorization': 'token ' + githubToken}};
+	return fetch(url, options)
+		.then(res => res.json())
+		.then(getLastPush)
+		.catch(console.error);
+}
+
+lastPushDate("christensencaleigh").then(console.log);
+
+// Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the
+// passed number of milliseconds. As a bonus make sure the promise resolves with the milliseconds in return, so you can make the console log message more dynamic.
+//  */
+
+function wait(numMilliseconds) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, numMilliseconds)
+	});
+}
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
