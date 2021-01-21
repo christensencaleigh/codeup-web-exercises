3e2f2cddf073933ead87e8ad41b5c45f4b394c93
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-21 16:48:43 -0600

    added weather icons

--- js/weather_map.js
+++ js/weather_map.js
@@ -9,8 +9,13 @@ $(document).ready(function () {
 			lon: lon,
 			units: "imperial"
 		}).done(function (data) {
+			//appending weather info to my cards in my html file
+
+			//need to refactor using a loop to clean up my code
+
 			$("#cardOne").append('<p class="card-header">'+ '</p>' + '<br>');
 			$("#cardOne").append('<p>' + data.daily[0].temp.max + '/' + data.daily[0].temp.min + '</p>');
+			$("#cardOne").append('<img src="http://openweathermap.org/img/wn/04d.png">');
 			$("#cardOne").append('<p>' + "Description: " + data.daily[0].weather[0].description + '</p>');
 			$("#cardOne").append('<p>' + "Humidity: " + data.daily[0].humidity + '</p>');
 			$("#cardOne").append('<p>' + "Wind: " + data.daily[0].wind_deg + '</p>');
@@ -18,6 +23,7 @@ $(document).ready(function () {
 
 			$("#cardTwo").append('<p class="card-header">' + '</p>' + '<br>');
 			$("#cardTwo").append('<p>' + data.daily[1].temp.max + '/' + data.daily[1].temp.min + '</p>');
+			$("#cardTwo").append('<img src="http://openweathermap.org/img/wn/04d.png">');
 			$("#cardTwo").append('<p>' + "Description: " + data.daily[1].weather[0].description + '</p>');
 			$("#cardTwo").append('<p>' + "Humidity: " + data.daily[1].humidity + '</p>');
 			$("#cardTwo").append('<p>' + "Wind: " + data.daily[1].wind_deg + '</p>');
@@ -25,6 +31,7 @@ $(document).ready(function () {
 
 			$("#cardThree").append('<p class="card-header">' + '</p>' + '<br>');
 			$("#cardThree").append('<p>' + data.daily[2].temp.max + '/' + data.daily[2].temp.min + '</p>');
+			$("#cardThree").append('<img src="http://openweathermap.org/img/wn/10d.png">');
 			$("#cardThree").append('<p>' + "Description: " + data.daily[2].weather[0].description + '</p>');
 			$("#cardThree").append('<p>' + "Humidity: " + data.daily[2].humidity + '</p>');
 			$("#cardThree").append('<p>' + "Wind: " + data.daily[2].wind_deg + '</p>');
@@ -32,6 +39,7 @@ $(document).ready(function () {
 
 			$("#cardFour").append('<p class="card-header">' + '</p>' + '<br>');
 			$("#cardFour").append('<p>' + data.daily[3].temp.max + '/' + data.daily[3].temp.min + '</p>');
+			$("#cardFour").append('<img src="http://openweathermap.org/img/wn/10d.png">');
 			$("#cardFour").append('<p>' + "Description: " + data.daily[3].weather[0].description + '</p>');
 			$("#cardFour").append('<p>' + "Humidity: " + data.daily[3].humidity + '</p>');
 			$("#cardFour").append('<p>' + "Wind: " + data.daily[3].wind_deg + '</p>');
@@ -39,14 +47,13 @@ $(document).ready(function () {
 
 			$("#cardFive").append('<p class="card-header">' + '</p>' + '<br>');
 			$("#cardFive").append('<p>' + data.daily[4].temp.max + '/' + data.daily[4].temp.min + '</p>');
+			$("#cardFive").append('<img src="http://openweathermap.org/img/wn/10d.png">');
 			$("#cardFive").append('<p>' + "Description: " + data.daily[4].weather[0].description + '</p>');
 			$("#cardFive").append('<p>' + "Humidity: " + data.daily[4].humidity + '</p>');
 			$("#cardFive").append('<p>' + "Wind: " + data.daily[4].wind_deg + '</p>');
 			$("#cardFive").append('<p>' + "Pressure: " + data.daily[4].pressure + '</p>');
 
-
-
-
+			//setting up my map
 
 
 
